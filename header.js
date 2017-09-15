0fc768110385d2c87c5b6c2d5ee30df3aac0434a
ThomasLDowns <thomasldowns@gmail.com>
2017-09-15 14:28:31 -0500

    Joyous Events 0.03.02
    Changed navbar menu on mobile, changed some spacing for header

--- header.js
+++ header.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     $(".navbar-hamburger").click(function(){
-        $(".hamburger-icon").stop().toggleClass("fa-bars");
-        $(".hamburger-icon").stop().toggleClass("fa-times");
+        $(".hamburger-icon").stop().toggleClass("fa-bars").toggleClass("fa-times");
         $(".navbar-container").stop().slideToggle(300);
     });
 });
