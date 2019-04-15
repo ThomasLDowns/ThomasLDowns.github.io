99a33e18c2e8bf23328c089141ecc7e160f22bd9
ThomasLDowns <thomasldowns@gmail.com>
2019-04-15 15:11:21 -0500

    Delete header.js

--- header.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $(".navbar-hamburger").click(function(){
-        $(".hamburger-icon").stop().toggleClass("fa-bars").toggleClass("fa-times");
-        $(".navbar-container").stop().slideToggle(300);
-    });
-});
